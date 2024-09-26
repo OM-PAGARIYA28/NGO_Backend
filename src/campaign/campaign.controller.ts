fa42dbea00a7ad1a7c7f6d6729ca24fb52daf764
Om Mayur Pagariya <ommayurpagariya@Oms-MacBook-Air.lan>
2024-09-26 12:33:18 +0530

    patch error2

--- src/campaign/campaign.controller.ts
+++ src/campaign/campaign.controller.ts
@@ -42,31 +42,35 @@ export class CampaignController {
 
   @UseGuards(JwtAuthGuard)
 @Patch('update/:id')
-@UseInterceptors(FileInterceptor('photo'))  // Intercept file upload for the 'photo' field
-async update(
-  @Request() req, 
-  @Param('id') id: string, 
-  @UploadedFile() file: Express.Multer.File,  // Handle the uploaded file
-  @Body() updateCampaignDto: UpdateCampaignDto
-) {
-  if (req.user.role !== 'ADMIN') {
-    throw new UnauthorizedException('Access restricted to admins');
-  }
-
-  let photoUrl: string | undefined;
-  if (file) {
-    // If a new photo is provided, upload it to Cloudinary
-    photoUrl = await this.cloudinaryService.uploadImage(file);
-  }
-
-  // Merge the updateCampaignDto with the new photo URL if it's provided
-  const updatedData = {
-    ...updateCampaignDto,
-    ...(photoUrl && { photo: photoUrl }),  // Only update the photo if a new one is uploaded
-  };
-
-  return this.campaignService.update(+id, updatedData);
+async temp(@Request() req,@Param('id') id: string, @Body() updateCampaignDto: UpdateCampaignDto){
+  console.log("hello")
+  return "hello"
 }
+// @UseInterceptors(FileInterceptor('photo'))  // Intercept file upload for the 'photo' field
+// async update(
+//   @Request() req, 
+//   @Param('id') id: string, 
+//   @UploadedFile() file: Express.Multer.File,  // Handle the uploaded file
+//   @Body() updateCampaignDto: UpdateCampaignDto
+// ) {
+//   if (req.user.role !== 'ADMIN') {
+//     throw new UnauthorizedException('Access restricted to admins');
+//   }
+
+//   let photoUrl: string | undefined;
+//   if (file) {
+//     // If a new photo is provided, upload it to Cloudinary
+//     photoUrl = await this.cloudinaryService.uploadImage(file);
+//   }
+
+//   // Merge the updateCampaignDto with the new photo URL if it's provided
+//   const updatedData = {
+//     ...updateCampaignDto,
+//     ...(photoUrl && { photo: photoUrl }),  // Only update the photo if a new one is uploaded
+//   };
+
+//   return this.campaignService.update(+id, updatedData);
+// }
 
   @UseGuards(JwtAuthGuard)
   @Delete(':id')

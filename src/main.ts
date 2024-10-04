326aa1f698a8b121e47d274f4fa45dac637d5c69
Om Mayur Pagariya <ommayurpagariya@Oms-MacBook-Air.local>
2024-10-04 09:11:28 +0530

    changed main Cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  app.enableCors();
+
+  // Enable global validation pipes for request validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Enable CORS and allow your frontend to access the API
+  app.enableCors({
+    origin: 'http://localhost:5173', // Allow your frontend React app
+    methods: 'GET,POST,PUT,DELETE',  // Define allowed methods
+    allowedHeaders: 'Content-Type, Authorization', // Allow these headers
+    credentials: true,  // If you're dealing with cookies/auth
+  });
+
   await app.listen(3000);
-  
 }
+
 bootstrap();
